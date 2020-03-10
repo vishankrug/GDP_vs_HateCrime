72e911e27459fddc64813dd9c9cd75339c71ede5
Jaimie Jin <jinjaimie12@gmail.com>
2020-03-09 20:41:59 -0700

    visual part 1 added

--- app_server.R
+++ app_server.R
@@ -1,6 +1,9 @@
 library("shiny")
 library("ggplot2")
 
+hate_crimes <- read.csv("data/hate_crimes.csv", stringsAsFactors = FALSE)
+gdp_by_state <- read.csv("data/gdp_by_state.csv", stringsAsFactors = FALSE)
+
 server <- function(input, output) {
   
 }
--- app_ui.R
+++ app_ui.R
@@ -1,3 +1,30 @@
 library("shiny")
 
-ui <- navbarPage(title = "")
+sidebar_content <- sidebarPanel(
+  sliderInput(
+    inputId = "year",
+    label = "Year",
+    min = 1997,
+    max = 2016,
+    value = c(1997, 2016)
+  )
+)
+
+main_content <- mainPanel(
+  plotOutput(
+    outputId = "plot"
+  )
+)
+
+change_in_GDP_over_time <- tabPanel(
+  title = "Change in GDP over time",
+  titlePanel("How has each state's GDP changed over time?"),
+  sidebarLayout(
+    sidebar_content,
+    main_content
+  )
+)
+
+
+ui <- navbarPage(title = "GDP and Hate Crimes",
+                 change_in_GDP_over_time)
